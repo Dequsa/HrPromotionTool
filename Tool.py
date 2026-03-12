5d3e25fc4cb8312abb68042c0f62ed3d774f7859
dequsa <marciniak.kacperek@gmail.com>
2026-03-12 22:22:43 +0100

    Fixed nan values for loss | added possibility to save model | WIP - CLI tool for worker promotion prediction

---
+++ Tool.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import pandas as pd
+import numpy as np
+
+# load model -> take user input -> parse user input -> run through model -> print the output
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+
+
+if __name__ == "__main__":
+    input_handler = InputHandler()
+    model_loader.load()
+
